f658f21f8dd982a55c00776d83cbf81b6c19048f
kavyapuretech <kavya.murhali@puretechltd.com>
2017-01-24 22:02:31 -0500

    Add files via upload

---
+++ js/window.js
@@ -0,0 +1,16 @@
+require(["dojo/dom", "dojo/on", "dojo/dom-style", "dojo/domReady!"],
+
+function (dom, on,domStyle) {
+    var isClicked = true;
+    on(dom.byId("Webnotes"), "click", function (e) {
+        if (isClicked === false) {
+        	var myNode = dom.byId("noteswindow");
+        	  domStyle.set(myNode, "display", "none");
+            isClicked = true;
+        } else {
+        	var myNode = dom.byId("noteswindow");
+      	  domStyle.set(myNode, "display", "block");
+            isClicked = false;
+        }
+    });
+});
