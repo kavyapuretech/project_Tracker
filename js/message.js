23eba46903a05d8e4a05fc3c530511be35d23e57
kavyapuretech <kavya.murhali@puretechltd.com>
2017-01-29 12:36:40 -0500

    Delete message.js

--- js/message.js
+++
@@ -1,5 +0,0 @@
-function iframeclick(){
-           document.getElementById("thisframe").contentWindow.document.body.onclck= function(){
-                document.getElementById("thisframe").contentWindow.location.reload();
-           }
-       }
